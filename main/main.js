ed00600e70cbbeb00ace8d256d943454dd1e8377
shahruk25 <shahruk25@gmail.com>
2022-01-03 22:57:40 +0530

    added is chain valid feature

--- main/main.js
+++ main/main.js
@@ -14,7 +14,7 @@ class Block {
     calculateHash(){
         // let encryted = SHA256(this.index + this.timestamp + this.perviousHash + JSON.stringify(this.data));
         // console.log("encryted",encryted);
-        // console.log("encryted toString",encryted.toString);
+        // console.log("encryted toString",encryted.toString());
         return SHA256(this.index + this.perviousHash + this.timestamp  + JSON.stringify(this.data)).toString();
     }
 
@@ -38,10 +38,26 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+    
+    isChainValid(){
+         for(let i=1; i < this.chain.length; i++) {
+             const currentBlock = this.chain[i];
+             const previousBlock = this.chain[i-1];
+            if(currentBlock.hash !== currentBlock.calculateHash()) return false;
+            if(currentBlock.perviousHash !== previousBlock.hash) return false;
+        }
+        return true;
+    }
 }
 
 let techMonkeyCoin = new Blockchain();
-techMonkeyCoin.addBlock(new Block(1,new Date,{amt:"techMonkey======> 1"}));
-techMonkeyCoin.addBlock(new Block(2,new Date,{amt:"techMonkey======> 1"}));
+techMonkeyCoin.addBlock(new Block(1,"03/01/2022",{amt:"techMonkey======> 1"}));
+techMonkeyCoin.addBlock(new Block(2,"02/01/2022",{amt:"techMonkey======> 2"}));
 
 console.log("techMonkeyCoin", JSON.stringify(techMonkeyCoin,null,4));
+console.log("Is block chain valid ??", techMonkeyCoin.isChainValid());
+console.log("temper again..!!");
+techMonkeyCoin.chain[1].data = {amt:"techMonkey======> 0"};
+console.log(techMonkeyCoin.chain[1]);
+console.log("check again..........Is block chain valid ??", techMonkeyCoin.isChainValid());
+
