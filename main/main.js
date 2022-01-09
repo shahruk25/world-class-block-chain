f16af726968e4633723ba6b81f60dedef1d642de
shahruk25 <shahruk25@gmail.com>
2022-01-09 22:43:39 +0530

    Mining & Transactions

--- main/main.js
+++ main/main.js
@@ -1,22 +1,28 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
 class Block {
 
-    constructor(index, timestamp, data, perviousHash='', nonce = 0){
+    constructor(timestamp, txn, perviousHash='', nonce = 0){
         // console.log("............Block................");
-        this.index = index;
         this.timestamp = timestamp;
-        this.data = data;
+        this.txn = txn;
         this.perviousHash = perviousHash;
         this.hash = this.calculateHash();
         this.nonce = nonce;
     }
 
     calculateHash(){
-        // let encryted = SHA256(this.index + this.timestamp + this.perviousHash + JSON.stringify(this.data));
+        // let encryted = SHA256(this.index + this.timestamp + this.perviousHash + JSON.stringify(this.txn));
         // console.log("encryted",encryted);
         // console.log("encryted toString",encryted.toString());
-        return SHA256(this.index + this.perviousHash + this.timestamp  + JSON.stringify(this.data) + this.nonce).toString();
+        return SHA256(this.index + this.perviousHash + this.timestamp  + JSON.stringify(this.txn) + this.nonce).toString();
     }
 
     mineBlock(difficulty){
@@ -32,22 +38,46 @@ class Blockchain{
     constructor(){
         // console.log("............BlockChain................");
         this.chain = [this.createGenesisBlock()];
-        this.difficulty =4;
+        this.difficulty = 2;
+        this.pendingTransaction = [];
+        this.miningRewards = 100;
     }
 
     createGenesisBlock(){
-        return new Block(0,new Date(), "techMonkey", "0");
+        return new Block(new Date(), "techMonkey", "0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length-1];
     }
 
-    addBlock(newBlock){
-        newBlock.perviousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    miningPendingTxn(miningRewardAddress){
+        let block =  new Block(Date.now,this.pendingTransaction);
+        block.mineBlock(this.difficulty);
+
+        console.log("Block successfully mined!");
+        this.chain.push(block);
+        this.pendingTransaction = [
+            new Transaction(null, miningRewardAddress,this.miningRewards)
+        ];
     }
+
+    createTransaction(txn){
+        this.pendingTransaction.push(txn);
+    }
+
+    getBalance(address){
+        let balance = 0;
+        for(const block of this.chain){
+             for(const txn of block.txn){
+                if(txn.fromAddress === address ) balance -=txn.amount;
+                if(txn.toAddress === address ) balance +=txn.amount;
+             }   
+        }
+        return balance;
+
+    }
+
     
     isChainValid(){
          for(let i=1; i < this.chain.length; i++) {
@@ -61,16 +91,15 @@ class Blockchain{
 }
 
 let techMonkeyCoin = new Blockchain();
-console.log("Mining block 1..");
-techMonkeyCoin.addBlock(new Block(1,"03/01/2022",{amt:"techMonkey======> 1"}));
+techMonkeyCoin.createTransaction(new Transaction("address1","address2",1000));
+techMonkeyCoin.createTransaction(new Transaction("address1","address2",500));
 
-console.log("Mining block 2..");
-techMonkeyCoin.addBlock(new Block(2,"02/01/2022",{amt:"techMonkey======> 2"}));
+console.log("\n Start mining ..");
+techMonkeyCoin.miningPendingTxn("techMonkey#Address");
 
-// console.log("techMonkeyCoin", JSON.stringify(techMonkeyCoin,null,4));
-// console.log("Is block chain valid ??", techMonkeyCoin.isChainValid());
-// console.log("temper again..!!");
-// techMonkeyCoin.chain[1].data = {amt:"techMonkey======> 0"};
-// console.log(techMonkeyCoin.chain[1]);
-// console.log("check again..........Is block chain valid ??", techMonkeyCoin.isChainValid());
+console.log("techMonkey balance = ",techMonkeyCoin.getBalance("techMonkey#Address"));
 
+console.log("\n Start mining again..");
+techMonkeyCoin.miningPendingTxn("techMonkey#Address");
+
+console.log("techMonkey balance = ",techMonkeyCoin.getBalance("techMonkey#Address"));
